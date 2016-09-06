79bf490ab864128ed0b6b527600d8536f45f6ced
Austin Boyd <austinnboyd93@gmail.com>
2016-09-06 18:44:24 -0400

    first edit

---
+++ myString/myString.cpp
@@ -0,0 +1,18 @@
+// myString.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	char name[50];
+	cout << "enter a name" << endl;
+	gets_s(name);
+	cout << "your name is: " << name << endl;
+	cout << "all done!!" << endl;
+    return 0;
+}
+
